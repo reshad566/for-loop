f8801e2494479f76a8f64e2da3d57804ec1ac686
reshad566 <reshadrahman601@gmail.com>
2025-07-26 20:49:26 +0600

    Add files via upload

---
+++ ifelse.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int i,sum=0;
+     for(i=0;i<=10;i++){
+          cout<<i<<endl;
+          
+          sum=sum+i;
+     }
+     cout<<"Sum of first 10 numbers is: "<<sum<<endl;
+     return 0;
+
+}
